e59fce2e0bd66348f9966cba1a0e2193ad0648f5
ProfRenatoPereira <105118481+ProfRenatoPereira@users.noreply.github.com>
2023-08-12 20:41:56 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -270,10 +270,8 @@ function incluiPlacar(){
   rect(0.9*width, 14, 40, 20);
   fill(255);
   text(pontosDoOponente, 0.917*width, 30);
-  fill(color(255, 140, 0));
-  rect(0.4*width, 14, 120, 20);
   fill(color(random(0, 256), random(0, 256), random(0, 256)));
-  text('Tela Cheia', 0.57*width, 30);
+  text('→TELA CHEIA←', 0.45*width, 30);
     }
 
 function marcaPonto(){
